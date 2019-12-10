37ad269e1858c11d1f0651dc5a158246d895c9c4
ephsmith <fasmith0229@gmail.com>
2019-12-10 09:01:29 -0600

    Update pacmanstart.ps1
    updated urls to match this forked repo

--- en/resources/pacmanstart.ps1
+++ en/resources/pacmanstart.ps1
@@ -5,10 +5,10 @@ foreach($d in $directories)
     New-Item -ItemType Directory -Force -Path $d
 }
 
-wget https://raw.githubusercontent.com/raspberrypilearning/pacman-terminal/master/en/resources/8linky.txt -OutFile "$HOME\Pictures\8linky.txt"
-wget https://raw.githubusercontent.com/raspberrypilearning/pacman-terminal/master/en/resources/PiNkY.txt -OutFile "$HOME\Desktop\PiNkY.txt"
-wget https://raw.githubusercontent.com/raspberrypilearning/pacman-terminal/master/en/resources/1nky.txt -OutFile "$HOME\Documents\1nky.txt"
-wget https://raw.githubusercontent.com/raspberrypilearning/pacman-terminal/master/en/resources/CLYDE.txt -OutFile "$HOME\Music\CLYDE.txt"
-wget https://raw.githubusercontent.com/raspberrypilearning/pacman-terminal/master/en/resources/sp00ky.txt -OutFile "$HOME\Documents\sp00ky.txt"
-wget https://raw.githubusercontent.com/raspberrypilearning/pacman-terminal/master/en/resources/funky.txt -OutFile "$HOME\Videos\funky.txt"
-wget https://raw.githubusercontent.com/raspberrypilearning/pacman-terminal/master/en/resources/sue.txt -OutFile "$HOME\Downloads\sue.txt"
+wget https://raw.githubusercontent.com/ephsmith/pacman-terminal/master/en/resources/8linky.txt -OutFile "$HOME\Pictures\8linky.txt"
+wget https://raw.githubusercontent.com/ephsmith/pacman-terminal/master/en/resources/PiNkY.txt -OutFile "$HOME\Desktop\PiNkY.txt"
+wget https://raw.githubusercontent.com/ephsmith/pacman-terminal/master/en/resources/1nky.txt -OutFile "$HOME\Documents\1nky.txt"
+wget https://raw.githubusercontent.com/ephsmith/pacman-terminal/master/en/resources/CLYDE.txt -OutFile "$HOME\Music\CLYDE.txt"
+wget https://raw.githubusercontent.com/ephsmith/pacman-terminal/master/en/resources/sp00ky.txt -OutFile "$HOME\Documents\sp00ky.txt"
+wget https://raw.githubusercontent.com/ephsmith/pacman-terminal/master/en/resources/funky.txt -OutFile "$HOME\Videos\funky.txt"
+wget https://raw.githubusercontent.com/ephsmith/pacman-terminal/master/en/resources/sue.txt -OutFile "$HOME\Downloads\sue.txt"
